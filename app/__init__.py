31811c371e7938f97bb7aebcf5d16e4737e8440d
Matthew <deadwalker1228@gmail.com>
2024-06-18 11:18:22 -0700

    Added image to new page

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/MatthewChang')
+def mc():
+    return render_template('mc.html', title="Matthew Chang", url=os.getenv("URL"))
