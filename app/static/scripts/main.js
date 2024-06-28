6d2af828a49b51c13e1d89e119e649954e77ac76
Matthew <deadwalker1228@gmail.com>
2024-06-28 11:00:42 -0700

    Refine CSS

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -8,6 +8,6 @@ function toggle_accordion(button) {
   if (panel.style.display === "block") {
     panel.style.display = "none";
   } else {
-    panel.style.display = "block";
+    panel.style.display = "flex";
   }
 }
