0e31a8f69ca5b40d45c65491246c96e282ec50d7
Vuong Ho <hochivuong2002@gmail.com>
2024-07-12 15:01:19 +0000

    fix: change redeploy site to using service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pkill -f tmux
 
 cd ~/MLH_Portfolio
 
@@ -12,7 +11,9 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 
-tmux new-session -d -s mlh_portfolio
-tmux send-keys 'flask run --host=0.0.0.0' C-m
-tmux detach -s mlh_portfolio
+systemctl start myportfolio
+systemctl enable myportfolio
+
+systemctl daemon-reload
+systemctl restart myportfolio
 
