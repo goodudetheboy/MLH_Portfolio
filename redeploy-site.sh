fd34460f1e51ff82315379a974488ed7d9911b35
Vuong Ho <hochivuong2002@gmail.com>
2024-07-12 15:06:57 +0000

    fix: Add comments for ./redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-
+# Change dir in to MLH Portfolio and fetch branch
 cd ~/MLH_Portfolio
-
 git fetch && git reset origin/main --hard
 
+# Activate virtual env and pip install requirements
 python -m venv venv
-
 source venv/bin/activate
-
 pip install -r requirements.txt
 
+# Start and enable myportfolio if no service is enabled yet
 systemctl start myportfolio
 systemctl enable myportfolio
 
+# Reload and restart service
 systemctl daemon-reload
 systemctl restart myportfolio
 
